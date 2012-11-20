c2928b1487f81abe15f0570409bc14671b24d3fe
Corentin Mario Barbu <corentin.barbu@gmail.com>
2012-11-19 20:41:53 -0500

    Removing unnessary files for PLoS comp streets art

--- archive/landscape_genetics.R
+++
@@ -1,9 +0,0 @@
-
-# import Data
-db.base <- read.csv("DB_mm_blocks_05May2011.csv",header=TRUE);
-
-# import necessary functions
-source("spam_complement.r")
-
-# select houses using lab_codes
-
--- example_PLoS_check.R
+++
@@ -1,7 +0,0 @@
-source("spatcontrol.R")
-graphics.off()
-samples<-trace.mcmc()
-estimates<-posteriors.mcmc(samples=samples)
-
-tb<-summary.spatcontrol(estimates=estimates)
-
