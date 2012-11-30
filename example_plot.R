5cf82fede331aa2f3e9561257f04b3863f3368d1
Corentin Mario Barbu <corentin.barbu@gmail.com>
2012-11-29 23:46:44 -0500

    Format of the file

--- example_plot.R
+++ example_plot.R
@@ -5,7 +5,6 @@ db <- read.csv("spatcontrol/JitteredDataPaucarpata.csv")
 # donde estan grupos de puntos (localidad, manzanas)
 plot.id(db$X,db$Y,db$GroupNum)
 
-
 # cuales son las valores en dos grupos
 fakeData<-cbind(rnorm(100,mean=1,sd=1),rnorm(100,mean=3,sd=2))
 boxplot.free(fakeData)
