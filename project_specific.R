ec67d3b100b27bc9ff21f147d2d260687f1cc5a5
Corentin Mario Barbu <corentin.barbu@gmail.com>
2012-12-21 11:43:26 -0700

    Add cleanPDLV

--- project_specific.R
+++ project_specific.R
@@ -7,5 +7,20 @@ unicodeToPDLV<-function(unicodes){
 make_unicode_gps<-function(unicodes){
 	return(gsub("[A-Z]$","",unicodes,ignore.case=TRUE))
 }
+# Clean P,D,L,V from spaces
+cleanPDLV<-function(Table){
+	Table$P<-gsub(" ","",Table$P)
+	Table$D<-gsub(" ","",Table$D)
+	Table$L<-gsub(" ","",Table$L)
+	Table$V<-gsub(" ","",Table$V)
+	Table$unicode<-paste(Table$P,Table$D,Table$L,Table$V,sep=".")
+	return(Table)
+}
+# clean unicodes from spaces and lower case
+cleanUnicodes<-function(unicodes){
+	unicodes<-gsub(" ","",unicodes)
+	unicodes<-toupper(bugs$UNICODE)
+	return(unicodes)
+}
 
 
