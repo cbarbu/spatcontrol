8bc8316a1c0830d5a6ca2f5febbe55156272bcf4
Karthik Sethuraman <k.seth1993@gmail.com>
2013-09-19 13:33:17 -0400

    Added ESS funct from LaplacesDemon

--- spatcontrol.R
+++ spatcontrol.R
@@ -7,7 +7,7 @@ library(testthat)
 library(Hmisc)
 library(plotrix)
 library("maptools")
-# library(LaplacesDemon)
+# library(LaplacesDemon) package discontinued on rcran, may need to find functions again
 library(locfit)
 library("binom")
 library(fields)
@@ -2300,6 +2300,42 @@ cb.diag<-function(sampBrut,baseLimitGeweke=0.05,KthinInit=1,logfile=""){
 	return(invisible(new("cb.diag.out",output,colAnalysed=colAnalysed,geweke=resG,limitGeweke=limitGeweke,raftery=resRaf)));
 }
 # Ex: cb.diag(a<-matrix(rnorm(10000),ncol=10))
+
+###########################################################################
+# ESS or Effective.Size                                                          
+# Taken from: LaplacesDemon v10.12.30                                                             
+#
+# The purpose of this function is to estimate the effective sample size   
+# of a target distribution after taking autocorrelation into account.    
+# Although the code is slightly different, it is essentially the same as 
+# the effectiveSize function in the coda package.
+# This function is called inside cb.diag and thus needs to be here
+###########################################################################
+ESS <- function(x){
+     x <- as.matrix(x)
+     v0 <- order <- numeric(ncol(x))
+     names(v0) <- names(order) <- colnames(x)
+     z <- 1:nrow(x)
+     for (i in 1:ncol(x))
+          {
+          lm.out <- lm(x[, i] ~ z)
+          if (identical(all.equal(sd(residuals(lm.out)), 0), TRUE)) {
+               v0[i] <- 0
+               order[i] <- 0
+               }
+          else {
+               ar.out <- ar(x[, i], aic = TRUE)
+               v0[i] <- ar.out$var.pred/(1 - sum(ar.out$ar))^2
+               order[i] <- ar.out$order
+               }
+          }
+     spec <- list(spec = v0, order = order)
+     spec <- spec$spec
+     Eff.Size <- ifelse(spec == 0, 0, nrow(x) * apply(x, 2, var)/spec)
+     Eff.Size <- ifelse(Eff.Size > nrow(x), nrow(x), Eff.Size)
+     return(Eff.Size)
+}
+
 
 # # To perform the Gelman-Rubin (from library(boa))
 # boa.chain.gandr(list(matrixOfParamChains1,matrixOfParamChains2),alpha=0.05)
